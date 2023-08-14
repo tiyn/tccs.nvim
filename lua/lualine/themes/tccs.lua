30eab32bfe1cdc0387ec7575787ef87b704874f0
tiyn <tiyn@mail-mk.eu>
2023-08-15 01:22:26 +0200

    changed lualine colors

--- lua/lualine/themes/tccs.lua
+++ lua/lualine/themes/tccs.lua
@@ -6,7 +6,6 @@ local c = require('tccs.colors').get_colors()
 local line_c = {
   fg_prim  = c.fg,
   bg_prim  = c.panel_bg,
-  fg_sec   = c.fg,
   bg_sec   = c.panel_shadow,
   visual   = c.operator,
   insert   = c.comment,
@@ -18,29 +17,29 @@ local line_c = {
 return {
   normal = {
     a = { fg = line_c.bg_prim, bg = line_c.insert, gui = 'bold' },
-    b = { fg = line_c.fg_prim, bg = config.opts.transparent and 'NONE' or line_c.bg_sec },
-    c = { fg = line_c.fg_sec, bg = config.opts.transparent and 'NONE' or line_c.bg_prim },
+    b = { fg = line_c.fg_prim, bg = line_c.bg_sec },
+    c = { fg = line_c.fg_prim, bg = line_c.bg_prim },
   },
   insert = {
     a = { fg = line_c.bg_prim, bg = line_c.normal, gui = 'bold' },
-    b = { fg = line_c.fg_prim, bg = config.opts.transparent and 'NONE' or line_c.bg_sec },
+    b = { fg = line_c.fg_prim, bg = line_c.bg_sec },
   },
   visual = {
     a = { fg = line_c.bg_prim, bg = line_c.visual, gui = 'bold' },
-    b = { fg = line_c.fg_prim, bg = config.opts.transparent and 'NONE' or line_c.bg_sec },
+    b = { fg = line_c.fg_prim, bg = line_c.bg_sec },
   },
   replace = {
     a = { fg = line_c.bg_prim, bg = line_c.replace, gui = 'bold' },
-    b = { fg = line_c.fg_prim, bg = config.opts.transparent and 'NONE' or line_c.bg_sec },
+    b = { fg = line_c.fg_prim, bg = line_c.bg_sec },
   },
   command = {
     a = { fg = line_c.bg_prim, bg = line_c.contrast, gui = 'bold' },
-    b = { fg = line_c.fg_prim, bg = config.opts.transparent and 'NONE' or line_c.bg_sec },
-    c = { fg = line_c.fg_sec, bg = config.opts.transparent and 'NONE' or line_c.bg_prim },
+    b = { fg = line_c.fg_prim, bg = line_c.bg_sec },
+    c = { fg = line_c.fg_prim, bg = line_c.bg_prim },
   },
   inactive = {
     a = { fg = line_c.fg_prim, bg = line_c.bg_sec, gui = 'bold' },
-    b = { fg = line_c.fg_prim, bg = config.opts.transparent and 'NONE' or line_c.bg_sec },
-    c = { fg = line_c.fg_prim, bg = config.opts.transparent and 'NONE' or line_c.bg_prim },
+    b = { fg = line_c.fg_prim, bg = line_c.bg_sec },
+    c = { fg = line_c.fg_prim, bg = line_c.bg_prim },
   },
 }
